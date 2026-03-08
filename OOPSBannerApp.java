3abc2c34cda9a97d58a7712bdb82cf1e21a5af97
Kartikeya-Chinmaya <kartikeyacr07@gmail.com>
2026-03-08 11:07:53 +0530

    feat: display OOPS as ASCII art banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,16 @@
+/**
+ * OOPSBannerApp - Displays OOPS as an ASCII art banner.
+ * @author Kartikeya-Chinmaya
+ * @version 2.0
+ */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(" ***   ***  ****   ****");
+        System.out.println("*   * *   * *   * *    ");
+        System.out.println("*   * *   * *   * *    ");
+        System.out.println("*   * *   * ****   *** ");
+        System.out.println("*   * *   * *         *");
+        System.out.println("*   * *   * *         *");
+        System.out.println(" ***   ***  *      *** ");
     }
 }
